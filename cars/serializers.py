e87b9f8e2be7586da1751265b981cbaa06c68221
vanko <ivan.tym4ak@gmail.com>
2024-04-30 16:40:45 +0300

    lesson2

---
+++ cars/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from cars.models import Car
+
+class CarSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    brand = serializers.CharField(max_length=50)
+    price = serializers.IntegerField()
+    year = serializers.IntegerField()
+
+
+    def create(self, validated_data):
+        car = Car.objects.create(**validated_data)
+        return car
+
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+            instance.save()
+            return instance
--- configs/urls.py
+++ configs/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
-from cars.views import CarTestView
+from cars.views import CarListCreateView, CarRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('cars', CarTestView.as_view()),
-    # path('cars/<int:pk>', CarTestView.as_view()),
-    # path('cars/<str:pk>', CarTestView.as_view()),
-    path('cars/<slug:pk>', CarTestView.as_view()),
+    path('cars', CarListCreateView.as_view()),
+    path('cars/<int:pk>', CarRetrieveUpdateDestroyView.as_view()),
 ]
