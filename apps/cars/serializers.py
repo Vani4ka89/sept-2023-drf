ed570cf9cc9a311cd21b3bde5078f6dcda343584
vanko <ivan.tym4ak@gmail.com>
2024-05-02 19:53:51 +0300

    lesson3-clean_code

--- apps/cars/serializers.py
+++ apps/cars/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
 from apps.cars.views import Car
 
-# Variant 2________________
-
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,30 +15,3 @@ class CarListSerializer(serializers.ModelSerializer):
         model = Car
 
         fields = ('id', 'brand', 'year')
-
-
-# Variant 1___________
-
-# class CarSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     brand = serializers.CharField(max_length=50)
-#     year = serializers.IntegerField()
-#     seats = serializers.IntegerField()
-#     body_type = serializers.CharField(max_length=30)
-#     engine = serializers.FloatField()
-#
-#     def create(self, validated_data):
-#         car = Car.objects.create(**validated_data)
-#         return car
-#
-#     def update(self, instance, validated_data):
-#         for key, value in validated_data.items():
-#             setattr(instance, key, value)
-#             instance.save()
-#             return instance
-
-
-# class CarListSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     brand = serializers.CharField(max_length=50)
-#     year = serializers.IntegerField()
